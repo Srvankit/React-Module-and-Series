a72778ef07d30e3958dfa29f55d22e7440a72e27
ankityadav.reh@gmail.com <167362173+Srvankit@users.noreply.github.com>
2026-03-14 22:20:10 +0530

    Added Mega Project

---
+++ BlogSite-MegaProject/src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import conf from '../conf.js'
+import { Client, Account, ID } from 'appwrite'
+
+export class AuthService {
+    client = new this.client();
+    account;
+    
+    constructor(){
+        this.client
+        .setEndoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+
+        this.account = new this.account(this.client);
+    }
+
+    async createAccount({email, password, name}){
+        try {
+           const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            if
+            (userAccount){
+                //call another method
+                return this.login({email,password});
+            }else{
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+         throw error   
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+           return await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new authService();
+
+export default AuthService
